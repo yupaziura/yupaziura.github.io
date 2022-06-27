b9ff018d974b1ef676463d62f18500c57a12f287
YuliiaPaziura <yuliiapaziura@gmail.com>
2022-06-27 18:13:49 +0300

    add contact information

--- src/pages/footer/footer.js
+++ src/pages/footer/footer.js
@@ -2,6 +2,10 @@
 import {footer, footerUA} from '../../db/db';
 
 // conponents
+import GitHubIcon from '@mui/icons-material/GitHub';
+import EmailIcon from '@mui/icons-material/Email';
+import TelegramIcon from '@mui/icons-material/Telegram';
+import FacebookIcon from '@mui/icons-material/Facebook';
 
 // styles
 import './footer.scss';
@@ -24,16 +28,20 @@ const Footer = (props) => {
             <div className={`footer footer${activeTheme}`} id='footer'>
             <div className="footer__contacts">
                     <div className="footer__contact footer__contact_email">
-                        <div className="footer__img"></div>
-                        <div >yuliiapaziura@gmail.com</div>
+                        <div className="footer__img"><EmailIcon/></div>
+                        <a href='mailto:yuliiapaziura@gmail.com' >yuliiapaziura@gmail.com</a>
                     </div>
                     <div className="footer__contact telegram">
-                        <div className="footer__img"></div>
+                        <div className="footer__img"><TelegramIcon/></div>
                         <a href='https://t.me/yupaziura' >Telegram</a>
                     </div>
                     <div className="footer__contact facebook">
-                        <div className="footer__img"></div>
+                        <div className="footer__img"><FacebookIcon/></div>
                         <a href='https://www.facebook.com/profile.php?id=100010341139078' >Facebook</a>
+                    </div>
+                    <div className="footer__contact github">
+                        <div className="footer__img"><GitHubIcon/></div>
+                        <a href='https://github.com/yupaziura' >Github</a>
                     </div>
                 </div>
 
