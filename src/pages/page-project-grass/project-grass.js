a66acb0440d72af66be2ec087bee11060553e6a1
YuliiaPaziura <yuliiapaziura@gmail.com>
2022-07-07 16:56:08 +0300

    unsuccesfull build

--- src/pages/page-project-grass/project-grass.js
+++ src/pages/page-project-grass/project-grass.js
@@ -19,28 +19,28 @@ const ProjectGrass = (props) => {
 
                 <TextArea theme={props.theme}>
                     <div className="sketchfab-embed-wrapper"> 
-                        <iframe className='frame' title="reduce" frameBorder="0"  mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking"  src="https://sketchfab.com/models/d5198959a113453e9eb68b8d2d63336a/embed?autostart=1&camera=0">
+                        <iframe className='frame' title="reduce" frameBorder="0"  mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking"  src={`https://sketchfab.com/models/d5198959a113453e9eb68b8d2d63336a/embed?autostart=1&camera=0`}>
                         </iframe>
                     </div>
                 </TextArea>
 
                 <TextArea theme={props.theme}>
                     <div className="sketchfab-embed-wrapper"> 
-                        <iframe className='frame' title="series" frameBorder="0"  mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking"  src="https://sketchfab.com/models/24eec5d40a6f4221adbab3c675fa6990/embed?autostart=1&camera=0">
+                        <iframe className='frame' title="series" frameBorder="0"  mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking"  src={`https://sketchfab.com/models/24eec5d40a6f4221adbab3c675fa6990/embed?autostart=1&camera=0`}>
                         </iframe>
                     </div>
                 </TextArea>
 
                 <TextArea theme={props.theme}>
                     <div className="sketchfab-embed-wrapper"> 
-                        <iframe className='frame' title="series" frameBorder="0"  mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking"  src="https://sketchfab.com/models/0c9632dacd1d44a083718cd95d671fd5/embed?autostart=1&camera=0">
+                        <iframe className='frame' title="series" frameBorder="0"  mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking"  src={`https://sketchfab.com/models/0c9632dacd1d44a083718cd95d671fd5/embed?autostart=1&camera=0`}>
                         </iframe>
                     </div>
                 </TextArea>
 
                 <TextArea theme={props.theme}>
                     <div className="sketchfab-embed-wrapper"> 
-                        <iframe className='frame' title="series" frameBorder="0"  mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking"  src="https://sketchfab.com/models/b8d77ee6d891427cbbcdb360de62f20f/embed?autostart=1&camera=0">
+                        <iframe className='frame' title="series" frameBorder="0"  mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking"  src={`https://sketchfab.com/models/b8d77ee6d891427cbbcdb360de62f20f/embed?autostart=1&camera=0`}>
                         </iframe>
                     </div>
                 </TextArea>
